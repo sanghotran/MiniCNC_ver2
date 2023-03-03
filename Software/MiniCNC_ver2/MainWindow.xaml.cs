d9f973fa7fa0a5a0be036872d7a2d4f132fc814a
Your Name <you@example.com>
2023-03-03 23:14:04 +0700

    update GUI

--- Software/MiniCNC_ver2/MainWindow.xaml.cs
+++ Software/MiniCNC_ver2/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,12 +20,63 @@ namespace MiniCNC_ver2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            firtLoad();
         }
+
+        #region Properites
+        private bool _IsStarted;
+        public bool IsStarted
+        {
+            get => _IsStarted;
+            set
+            {
+                _IsStarted = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool _IsPaused;
+        public bool IsPaused
+        {
+            get => _IsPaused;
+            set
+            {
+                _IsPaused = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool _IsConnected;
+        public bool IsConnected
+        {
+            get => _IsConnected;
+            set
+            {
+                _IsConnected = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Functions
+        public void firtLoad()
+        {
+            IsStarted = false;
+            IsPaused = false;
+            IsConnected = false;
+        }
+        #endregion
 
         #region Event
         private void DragWindow(object sender, MouseButtonEventArgs e)
@@ -54,6 +107,18 @@ namespace MiniCNC_ver2
         private void Home(object sender, MouseButtonEventArgs e)
         {
 
+        }
+        private void Start(object sender, MouseButtonEventArgs e)
+        {
+            IsStarted = !IsStarted;
+        }
+        private void Pause(object sender, MouseButtonEventArgs e)
+        {
+            IsPaused = !IsPaused;
+        }
+        private void Connect(object sender, MouseButtonEventArgs e)
+        {
+            IsConnected = !IsConnected;
         }
         #endregion
 
